e46675e100c2170d7728ee5e866e0b26df6137fc
Alex Dine <alexdine1996@gmail.com>
2021-03-28 18:39:05 -0400

    Final editor version + build

---
+++ Pinball_GAME/PinballArcadeGame/Pinball GAME/Assets/Scripts/POWERBAR.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class POWERBAR : MonoBehaviour
+{
+    public GameObject launcher;
+
+
+    void Update()
+    {
+        this.GetComponent<UnityEngine.UI.Slider>().value = launcher.GetComponent<LauncherBehaviour>().power;
+    }
+}
+
